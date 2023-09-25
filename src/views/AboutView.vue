4ead0e1543868378c81d51153f96b2cc8a96276a
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-09-25 20:03:23 +0500

    nav

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="logo">
+      <a href="#">PLATFORM LOGO</a>
+    </div>
+    <div class="nav-link">
+      <a href="#">Результаты</a>
+      <a href="#">Возможности</a>
+      <a href="#">О платформе</a>
+      <a href="#">Вопросы ответы</a>
+      <a href="#">Задать вопрос</a>
+      <a href="#">Новости</a>
+      <a href="#">Контакты</a>
+    </div>
+    <div><a href="#">Вход/Регистрация</a></div>
   </div>
 </template>
 
